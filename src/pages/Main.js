96d5727fb491839fd779938d0609e560e2182b76
Rommel Santos <rommelrenne@gmail.com>
2020-08-05 23:11:32 -0300

    build: Added main logic of the app

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 
 export default class Main extends Component {
+  state = {
+    ddd: "",
+    telefone: "",
+  };
+
+  sendMessage = () => {
+    console.log(
+      `O ddd é ${this.state.ddd} e o número é ${this.state.telefone}`
+    );
+  };
+
   render() {
     return (
       <div>
-        <input placeholder='DDD'></input>
-        <input placeholder='telefone'></input>
-        <button>Enviar</button>
+        <input
+          placeholder='DDD'
+          onInput={(d) => this.setState({ ddd: d.target.value })}
+        ></input>
+        <input
+          placeholder='telefone'
+          onInput={(tel) => this.setState({ telefone: tel.target.value })}
+        ></input>
+        <button onClick={this.sendMessage}>Enviar</button>
       </div>
     );
   }
