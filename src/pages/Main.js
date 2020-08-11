f63e19fa0af0c1e057bfabf89a2dc030f4d01c88
Rommel Santos <rommelrenne@gmail.com>
2020-08-10 22:37:39 -0300

    build: Added some styles

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./style.css";
 
 export default class Main extends Component {
   state = {
@@ -7,23 +8,30 @@ export default class Main extends Component {
   };
 
   sendMessage = () => {
-    console.log(
-      `O ddd é ${this.state.ddd} e o número é ${this.state.telefone}`
+    window.open(
+      `https://api.whatsapp.com/send?1=ptBR&phone=55${this.state.ddd}${this.state.telefone}`
     );
   };
 
   render() {
     return (
-      <div>
-        <input
-          placeholder='DDD'
-          onInput={(d) => this.setState({ ddd: d.target.value })}
-        ></input>
-        <input
-          placeholder='telefone'
-          onInput={(tel) => this.setState({ telefone: tel.target.value })}
-        ></input>
-        <button onClick={this.sendMessage}>Enviar</button>
+      <div className='app'>
+        <div className='input-area'>
+          <input
+            className='ddd-input'
+            placeholder='DDD'
+            onInput={(d) => this.setState({ ddd: d.target.value })}
+          ></input>
+          <input
+            className='tel-input'
+            placeholder='Telefone'
+            onInput={(tel) => this.setState({ telefone: tel.target.value })}
+          ></input>
+        </div>
+        <br></br>
+        <div className='send'>
+          <button onClick={this.sendMessage}>Enviar</button>
+        </div>
       </div>
     );
   }
